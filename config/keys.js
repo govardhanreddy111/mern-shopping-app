011fe65a36b409898e4c9cae30b1ac8391ad9c43
Govardhan Reddy <bobbiligovardhanreddy@gmail.com>
2018-11-16 12:09:37 +0530

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-   // mongoURI : 'mongodb://localhost:27017/mern_shopping_cart'
-    mongoURI : 'mongodb://govardhan:govardhan1@ds235788.mlab.com:35788/mern_shopping_cart'
+    mongoURI : 'mongodb://localhost:27017/mern_shopping_cart'
+   // mongoURI : 'mongodb://govardhan:govardhan1@ds235788.mlab.com:35788/mern_shopping_cart'
 }
